5b17892498be2fb0feaebdf22c84cff07164f537
kvadeck <kvadeck@yandex.ru>
2021-03-03 18:59:09 +0300

    feat: timer fail refactor, start recipe

--- src/components/helpers/index.js
+++ src/components/helpers/index.js
@@ -25,40 +25,46 @@ export function buttonIconSwitcher(iconName) {
     }
 }
 
-export function getAllElementsWithAttribute(attribute)
-{
-  var matchingElements = [];
-  var allElements = document.getElementsByTagName('*');
-  for (var i = 0, n = allElements.length; i < n; i++)
-  {
-    if (allElements[i].getAttribute(attribute) !== null)
-    {
-      matchingElements.push(allElements[i]);
+export function getAllElementsWithAttribute(attribute) {
+    var matchingElements = [];
+    var allElements = document.getElementsByTagName('*');
+    for (var i = 0, n = allElements.length; i < n; i++) {
+        if (allElements[i].getAttribute(attribute) !== null) {
+            matchingElements.push(allElements[i]);
+        }
     }
-  }
-  return matchingElements;
+    return matchingElements;
 }
 
 export function findActiveCup(cups) {
     const activedCup = []
-    cups.forEach((cup)=> { activedCup.push(cup.dataset.active);})
+    cups.forEach((cup) => { activedCup.push(cup.dataset.active); })
     const cupIdx = activedCup.indexOf('true');
     return cupIdx;
-}
-
-export function findActiveCooking(cups, stage) {
-    const activedCooking = []
-    cups.forEach((cup)=> { activedCooking.push(cup.dataset.cooking);})
-    const cookingIdx = activedCooking.lastIndexOf(stage);
-    if(cookingIdx !== -1) {
-        return cookingIdx;
-    } else {
-        console.log('data-cooking not found');
-    }
-    
 }
 
 export function coffeeBrew(audioIdx) {
     audioIdx = new Audio(sound.coffeeBrew);
     return audioIdx;
+}
+
+export function stopPlay(audio) {
+    audio.pause();
+    audio.currentTime = 0.0;
+}
+
+export function buttonStateSwitcher(dataset) {
+    switch (dataset) {
+        case 'ready':
+        case 'done':
+            return 'sucess';
+        case 'fail':
+            return 'fail'
+        case 'start':
+            return 'disabled'
+        case 'active':
+            return 'start'
+        default:
+            return console.log('buttonStateSwitcher fnc error!');
+    }
 }
